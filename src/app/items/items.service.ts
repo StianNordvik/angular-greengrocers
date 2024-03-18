12b0095e396b1e7ec2a776da3e6b9d9831ed9dde
Stian Andreas Ytterstad Nordvik <stian.andreas.ytterstad.nordvik@no.experis.com>
2024-03-18 09:07:10 +0100

    Stian Andreas Ytterstad Nordvik C#3

--- src/app/items/items.service.ts
+++ src/app/items/items.service.ts
@@ -14,6 +14,6 @@ export class ItemsService {
 
   get items(): Promise<Item[]> {
     // @ts-ignore
-    return firstValueFrom(this.http.get(`${environment.apiUrl}groceries`))
+    return firstValueFrom(this.http.get<Item[]>(`${environment.apiUrl}groceries`))
   }
 }
--- src/app/items/list/list.component.ts
+++ src/app/items/list/list.component.ts
@@ -10,6 +10,7 @@ import { Item } from 'src/app/models/item';
 export class ListComponent {
   constructor(private readonly itemsService: ItemsService){}
   items = this.getItems();
+  filter = "All";
 
   getItems() {
     return this.itemsService.items;
@@ -17,7 +18,37 @@ export class ListComponent {
 
   addItem(item: any){
     this.itemsService.cartItems.push(item);
-    console.log(this.itemsService.cartItems)
+    console.log(this.itemsService.cartItems);
+  }
+
+  updateFilter(filterBy: string): void {
+    this.filter = filterBy;
+    this.items = this.filterItems();
+
+  }
+
+  async filterItems(){
+    if(this.filter === "fruit"){
+      return (await this.getItems()).filter(item => item.type === "fruit");
+    }
+    if(this.filter === "vegetable"){
+      return (await this.getItems()).filter(item => item.type === "vegetable");
+    }
+    else {
+      return this.getItems();
+    }
+  }
+
+  async sortItems(sortBy: string): Promise<void> {
+    this.items = this.filterItems();
+    if(sortBy === "price"){
+      await (await this.getItems()).sort((a, b) => a.price - b.price);
+    }
+    if(sortBy === "name"){
+      await (await this.getItems()).sort((a, b) => a.name.localeCompare(b.name));
+
+    }
+
   }
 
   
